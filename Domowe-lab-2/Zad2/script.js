289ac0bf816cbb04441ec1ad77a6570a32ae7597
Grzegorz <grzegorzpiskorski971@gmail.com>
2023-03-11 17:30:31 +0100

    Main functions implemented

--- Domowe-lab-2/Zad2/script.js
+++ Domowe-lab-2/Zad2/script.js
@@ -6,21 +6,69 @@ function sum(x,y) {
 }
 
 function cyfry(napis) {
+    let res = [];
+    let evenSum = 0;
+    let oddSum = 0;
+    
+    for (let character of napis) {
+        let sth = parseInt(character);
 
+        if (isNaN(sth)) {
+            continue;
+        } else if (sth % 2 === 0) {
+            evenSum += sth;
+        } else {
+            oddSum += sth;
+        }
+    }
+    
+    res.push(oddSum);
+    res.push(evenSum);
+    return res;
 }
 
-function litery(napis) {
 
+function litery(napis) {
+    let res = [];
+    let lowerCaseLetters = 0;
+    let upperCaseLetters = 0;
+
+    for (let character of napis) {
+        if (!(character.toLowerCase() != character.toUpperCase())) {
+            continue;
+        }
+
+        if (character === character.toLowerCase()) {
+            lowerCaseLetters++;
+        } else {
+            upperCaseLetters++;
+        }
+    }
+
+    res.push(lowerCaseLetters);
+    res.push(upperCaseLetters);
+    return res;
 }
 
 function suma(napis) {
+    if (isNaN(parseInt(napis[0]))) {
+        return 0;
+    }
 
+    return parseInt(napis);
 }
 
 function getData() {
     let data = window.prompt("Wczytaj dane:");
+    let prevSum = 0;
     while (data != null) {
-        
+        let currSum = prevSum + suma(data);
+        console.log(data);
+        console.log(cyfry(data));
+        console.log(litery(data));
+        console.log(currSum);
+        prevSum = currSum;
+        data = window.prompt("Wczytaj dane:");
     }
 }
 
